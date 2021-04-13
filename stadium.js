ddd857a72a9842688e7d6da250e25590ffe1f18d
Choi Yoochan <yoochan1012@gmail.com>
2021-04-14 01:27:42 +0900

    first commit

---
+++ stadium.js
@@ -0,0 +1,15 @@
+function Seoul () {
+    document.getElementById("Stadium").innerHTML =
+    "서울종합운동장 야구장 <br> &nbsp; ####년 완공, ##,###석 <br> &nbsp; 홈팀: ";
+    document.getElementById("map").src = "map.png";
+    document.getElementById("spe").innerHTML =
+    "규모: ***-***-*** (미터) <br> &nbsp; 그 외의 특색: "
+}
+
+function SD () {
+    document.getElementById("Stadium").innerHTML =
+    "고척 스카이돔 <br> &nbsp; ####년 완공, ##,###석 <br> &nbsp; 홈팀: ";
+    document.getElementById("map").src = "map.png";
+    document.getElementById("spe").innerHTML =
+    "규모: ***-***-*** (미터) <br> &nbsp; 그 외의 특색: "
+}
