1f923d4215d278ab9cd304a5c88db288a2ccea99
yoochan1012 <82464095+yoochan1012@users.noreply.github.com>
2021-04-21 18:08:29 +0900

    Add files via upload

---
+++ yoochan-vlog-main/color.js
@@ -0,0 +1,11 @@
+function green()
+{     document.body.style.backgroundColor="#33cc33";
+}
+
+function red() {
+    document.body.style.backgroundColor="#d32a09";
+}
+
+function blue() {
+    document.body.style.backgroundColor="#1F7CDC";
+}
